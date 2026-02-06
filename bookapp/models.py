4c6c39135830c7cab1ae4e3b3d22c98538dfd7cc
gpdzvapatsva <gpdzvapatsva@gmail.com>
2026-02-06 15:08:26 +0000

    new tables added

--- bookapp/models.py
+++ bookapp/models.py
@@ -12,3 +12,17 @@ class Books(models.Model):
         return f'Title : {self.title}, Author: {self.author}, Genre : {self.genre}, Price : {self.price}, Stock : {self.stock} '
 
 
+class Reader(models.Model):
+    name = models.CharField(max_length=60)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+class BookAssignment(models.Model):
+    reader = models.ForeignKey(Reader, on_delete=models.CASCADE)
+    book = models.ForeignKey(Books, on_delete=models.CASCADE)
+    assigned_date = models.DateField(auto_now_add=True)
+    returned_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.reader} → {self.book.title}"
